4d89d898129673f9aea4ca2a8cecca631341d3c1
yume045 <yume045@hotmail.com>
2018-10-30 16:19:41 +0700

    sss

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div id="app">
-    <section>
-        <b-tabs position="is-centered" class="block">
-            <b-tab-item label="Pictures"></b-tab-item>
-            <b-tab-item label="Music"></b-tab-item>
-            <b-tab-item label="Videos"></b-tab-item>
-        </b-tabs>
-
-        <b-tabs position="is-right" class="block">
-            <b-tab-item label="Pictures"></b-tab-item>
-            <b-tab-item label="Music"></b-tab-item>
-            <b-tab-item label="Videos"></b-tab-item>
-        </b-tabs>
-    </section>
     <router-view/>
   </div>
 </template>
