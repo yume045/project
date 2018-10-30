09621bff1eaa90a538a98ccefa547b5bdf9846e9
yume045 <yume045@hotmail.com>
2018-10-30 16:43:58 +0700

    Question

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -11,6 +11,43 @@
       <button style="margin-left:1%;" class="btn btn-outline-success"><router-link to="/Register">สมัครสมาชิก</router-link></button>
     </b-nav>
 </div><br><br>
+<nav class="navbar is-white">
+  <div class="container">
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item is-active" href="#">Popular</a>
+        <a class="navbar-item" href="#">Recent</a>
+        <a class="navbar-item" href="#">Rising</a>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <input class="input" type="search" placeholder="Search forum...">
+            </div>
+            </div>
+            </div>
+            </div>
+            </nav>
+            <section class="container">
+              <div class="columns">
+                <div class="column is-3">
+                  <a class="button is-primary is-block is-alt is-large" href="#">New Post</a>
+                  <aside class="menu">
+                    <p class="menu-label">
+                      Tags
+                      </p>
+                      <ul class="menu-list">
+                        <li><span class="tag is-primary is-medium ">Dashboard</span></li><br>
+                        <li><span class="tag is-link is-medium ">Customers</span></li><br>
+                        <li><span class="tag is-light is-danger is-medium ">Authentication</span></li><br>
+                        <li><span class="tag is-dark is-medium ">Payments</span></li><br>
+                        <li><span class="tag is-success is-medium ">Transfers</span></li><br>
+                        <li><span class="tag is-warning is-medium ">Balance</span></li><br>
+                        <li><span class="tag is-medium ">Question</span></li>
+                      </ul>
+                  </aside>
+                </div>
+              </div>
+            </section>
   </div>
 </template>
 <script>
@@ -40,4 +77,64 @@ li {
 a {
   color: #42b983;
 }
+html,body {
+  font-family: 'Open Sans', serif;
+  background: #F2F6FA;
+}
+footer {
+  background-color: #F2F6FA !important;
+}
+.topNav {
+  border-top: 5px solid #3498DB;
+}
+.topNav .container {
+  border-bottom: 1px solid #E6EAEE;
+}
+.container .columns {
+  margin: 3rem 0;
+}
+.navbar-menu .navbar-item {
+  padding: 0 2rem;
+}
+aside.menu {
+  padding-top: 3rem;
+}
+aside.menu .menu-list {
+  line-height: 1.5;
+}
+aside.menu .menu-label {
+  padding-left: 10px;
+  font-weight: 700;
+}
+.button.is-primary.is-alt {
+  background: #00c6ff;
+  background: -webkit-linear-gradient(to bottom, #0072ff, #00c6ff);
+  background: linear-gradient(to bottom, #0072ff, #00c6ff);
+  font-weight: 700;
+  font-size: 14px;
+  height: 3rem;
+  line-height: 2.8;
+}
+.media-left img {
+  border-radius: 50%;
+}
+.media-content p {
+  font-size: 14px;
+  line-height: 2.3;
+  font-weight: 700;
+  color: #8F99A3;
+}
+article.post {
+  margin: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #E6EAEE;
+}
+article.post:last-child {
+  padding-bottom: 0;
+  border-bottom: none;
+}
+.menu-list li{
+  padding: 5px;
+}
+
 </style>
