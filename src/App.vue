d5c689baaff37896b68a038896b9e4712d8a2869
yume045 <yume045@hotmail.com>
2018-10-30 22:46:58 +0700

    edit css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-lg navbar navbar-light" style="background-color: #00BFFF;">
+  <a class="navbar-brand" href="/#/">Medical Service</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="/">หน้าหลัก <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/#/">หน้าหลัก <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/#/Question">ปรึกษาแพทย์</a>
@@ -21,8 +21,8 @@
       </li>
     </ul>
   </div>
-      <a href="/#/Login"><button style="margin-left:0%;" class="btn btn-outline-primary">เข้าสู่ระบบ</button></a>
-      <a href="/#/Register"><button style="margin-left:2%;" class="btn btn-outline-primary">สมัครสมาชิก</button></a>
+      <a class="nav-link disabled " href="/#/Login">เข้าสู่ระบบ</a>
+      <a class="nav-link disabled " href="/#/Register">สมัครสมาชิก</a>
 </nav>
     <router-view/>
   </div>
